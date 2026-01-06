dff8f6ea11b3591cd17348af015b0716f261c220
gokulkalai333 <gokulkalai3333@gmail.com>
2026-01-06 10:59:27 +0530

    refactor: remove unused BackgroundAudio component from App

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ import Story from './components/Story'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
 import EventDetails from './pages/EventDetails'
-import BackgroundAudio from './components/BackgroundAudio'
 
 const ScrollToHash = () => {
   const { hash, pathname } = useLocation();
@@ -131,7 +130,6 @@ const Home = () => {
 const App = () => {
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden'>
-      <BackgroundAudio />
       <Navbar />
       <ScrollToHash />
       <Routes>
