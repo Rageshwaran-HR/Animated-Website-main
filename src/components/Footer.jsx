eb3fa0118ef1207ef5e22617461765f7174291af
vishvaa-vsk <kvishvaa6@gmail.com>
2026-01-30 23:18:55 +0530

    Update footer background to a gradient from blue-300 to blue-400

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -101,7 +101,7 @@ const Footer = () => {
   };
 
   return (
-    <footer id="contact" className="w-screen bg-violet-500 py-6 text-black">
+    <footer id="contact" className="w-screen bg-gradient-to-r from-blue-300 to-blue-400 py-6 text-black">
       <div className="container mx-auto px-4">
         <div
           ref={locationRef}
